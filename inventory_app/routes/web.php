13d2634ebcc78586358df10c1dd13417c0915c1a
blexiss <edgardimas737@gmail.com>
2025-08-12 11:06:09 +0800

    Start

---
+++ inventory_app/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('inventory');
+});
+
+Route::get('/inventory', function () {
+    return view('inventory');
+});
+
