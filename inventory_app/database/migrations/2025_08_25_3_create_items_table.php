3b413c118b5a8a0375b8bc1a8baec77ac6231ca3
WicfuL <wicful.15@gmail.com>
2025-08-26 14:31:27 +0800

    qty update 1.2

--- inventory_app/database/migrations/2025_08_25_3_create_items_table.php
+++ inventory_app/database/migrations/2025_08_25_3_create_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->string('name');
             $table->foreignId('subcategory_id')->constrained()->onDelete('cascade');
             $table->integer('quantity')->default(0);
-            $table->string('json')->nullable();
+            $table->longText('json')->nullable();
             $table->date('date_of_arrival')->nullable();
             $table->timestamps();
         });
